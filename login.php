ee99cf482156b0b0252fab5598a585d4f6bc76b0
Thomas Herr <thom1810@gmail.com>
2018-03-14 23:19:20 -0400

    Fixing login issues

--- login.php
+++ login.php
@@ -18,19 +18,21 @@
 <?php session_start(); ?>
 
 <body class="text-center">
-<form class="form-signin" action="/validate.php" method="post">
+<form class="form-signin" action="validate.php" method="post" accept-charset='UTF-8'>
 	<img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
 	<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
 	<label for="inputEmail" class="sr-only">Email address</label>
-	<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+
+    <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
 	<label for="inputPassword" class="sr-only">Password</label>
-	<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+    <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
 	<div class="checkbox mb-3">
 		<label>
 			<input type="checkbox" value="remember-me"> Remember me
 		</label>
 	</div>
-	<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	<button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">Sign in</button>
 	<p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
 </form>
 </body>
